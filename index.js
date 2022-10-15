7d6383623e5ec2986014c7abc5833a73391288e5
Daria Petlina <dsbaranova@gmail.com>
2022-10-15 23:07:12 +0600

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,17 +8,32 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to the server');
-    
+
     Dishes.create({
         'name': 'Dish 1',
         'description': 'descr'
     })
         .then((dish) => {
             console.log(dish);
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                    $set: {description: 'Updated test'}
+                },
+                {new: true}
+            ).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating: 5,
+                comment: 'Super',
+                author: 'Daria'
+            });
+
+           return dish.save();
+
+        })
+        .then((dish) => {
+            console.log(dish);
             return Dishes.deleteMany({});
         })
         .then(() => {
